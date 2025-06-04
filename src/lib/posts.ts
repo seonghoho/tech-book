f6e2040728ca89e4102df85c69ea4f28eda62313
choiseongho <chltjdgh3@naver.com>
2025-06-05 00:21:38 +0900

    feat: tech-book 구조 생성

---
+++ src/lib/posts.ts
@@ -0,0 +1,34 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import rehypeSlug from "rehype-slug";
+import rehypeStringify from "rehype-stringify";
+import remarkGfm from "remark-gfm";
+
+const postsDirectory = path.join(process.cwd(), "posts");
+
+export async function getPostData(slug: string) {
+  const fullPath = path.join(postsDirectory, `${slug}.md`);
+  const fileContents = fs.readFileSync(fullPath, "utf8");
+  const { data, content } = matter(fileContents);
+
+  const processedContent = await unified()
+    .use(remarkParse)
+    .use(remarkGfm)
+    .use(remarkRehype)
+    .use(rehypeSlug)
+    .use(rehypeStringify)
+    .process(content);
+
+  const contentHtml = processedContent.toString();
+
+  return {
+    slug,
+    contentHtml,
+    rawMarkdown: content,
+    ...(data as { title: string; date: string }),
+  };
+}
