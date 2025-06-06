1a78d18cf8e3786a547022c9d5f6db8acfbe6650
choiseongho <chltjdgh3@naver.com>
2025-06-06 11:00:32 +0900

    fix: 반응형 구조 적용

--- src/components/PostIndex.tsx
+++ src/components/PostIndex.tsx
@@ -7,7 +7,7 @@ type Heading = {
 export default function PostIndex({ headings }: { headings: Heading[] }) {
   return (
     <div>
-      <ul className="space-y-1 text-sm">
+      <ul className="space-y-1 text-sm p-4">
         {headings.map((h) => (
           <li key={h.id} className={`ml-${(h.depth - 2) * 4}`}>
             <a href={`#${h.id}`} className="hover:underline text-blue-600">
