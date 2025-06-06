b47378073c0144ca5274a7244762d7c8e4652c97
choiseongho <chltjdgh3@naver.com>
2025-06-06 23:07:31 +0900

    fix: client 파일과 server 파일 분리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,8 @@
-import { getAllPosts } from "@/lib/getAllPosts";
-import Link from "next/link";
+import { getPostsByCategory } from "@/lib/getPostsByCategory";
+import ClientHome from "@/components/ClientHome";
 
-export default async function Home() {
-  const posts = getAllPosts(); // 동기 함수이면 이대로 사용 가능
+export default function Home() {
+  const postsByCategory = getPostsByCategory(); // 서버에서 동기적으로 호출
 
-  return (
-    <main className="p-8">
-      <h1 className="text-2xl font-bold mb-6">📚 블로그 글 목록</h1>
-      <ul className="space-y-2">
-        {posts.map((post) => (
-          <li key={post.slug}>
-            <Link href={`/posts/${post.slug}`}>
-              <div className="text-blue-600 hover:underline cursor-pointer">
-                {post.title}{" "}
-                <span className="text-sm text-gray-500">({post.date})</span>
-              </div>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </main>
-  );
+  return <ClientHome data={postsByCategory} />;
 }
