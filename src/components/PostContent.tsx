4ae6c00de5a4e4dac7b0f86fbe207fe658087c20
choiseongho <chltjdgh3@naver.com>
2025-06-09 01:01:41 +0900

    fix: scroll 및 flex css 수정

--- src/components/PostContent.tsx
+++ src/components/PostContent.tsx
@@ -6,13 +6,15 @@ type Props = {
 
 export default function PostContent({ title, date, contentHtml }: Props) {
   return (
-    <article className="prose dark:prose-invert max-w-none">
-      <h1>{title}</h1>
-      <p className="prose text-sm dark:prose-invert">{date}</p>
-      <div
-        className="prose dark:prose-invert"
-        dangerouslySetInnerHTML={{ __html: contentHtml }}
-      />
+    <article className="prose dark:prose-invert w-full max-w-none">
+      <div className="max-w-full lg:max-w-screen-lg w-full lg:px-8 mx-auto">
+        <h1>{title}</h1>
+        <p className="text-sm text-gray-500 dark:text-gray-400">{date}</p>
+        <div
+          className="prose dark:prose-invert max-w-none"
+          dangerouslySetInnerHTML={{ __html: contentHtml }}
+        />
+      </div>
     </article>
   );
 }
