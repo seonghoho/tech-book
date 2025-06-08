9c3b2d5b980a4d50b8d36b57089cea7b321743ab
choiseongho <chltjdgh3@naver.com>
2025-06-08 15:21:38 +0900

    fix: category 한글로 표시하기 위한 객체 추가

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { PostMeta } from "@/lib/getPostsByCategory";
 import { useState, useRef } from "react";
 import { ChevronDownIcon, ChevronRightIcon } from "@/assets/svg";
+import { categoryMap } from "@/lib/categoryMap";
 
 type Props = {
   data: Record<string, PostMeta[]>;
@@ -19,16 +20,16 @@ export default function Sidebar({ data }: Props) {
 
   return (
     <div className="space-y-4">
-      <h2 className="text-xl font-bold mb-2">카테고리별 글</h2>
+      {/* <h2 className="text-lg font-bold mb-2">카테고리별 글</h2> */}
       {Object.entries(data).map(([category, posts]) => {
         const isOpen = openCategory === category;
         return (
-          <div key={category}>
+          <div key={category} className="text-[14px]">
             <button
               className="flex justify-between items-center w-full px-2 py-2 hover:bg-gray-100 dark:hover:bg-zinc-800 rounded text-left font-medium"
               onClick={() => toggleCategory(category)}
             >
-              <span>{category}</span>
+              <span>{categoryMap[category] ?? category}</span>
               {isOpen ? (
                 <ChevronDownIcon className="w-4 h-4 text-[#0F1B2A] dark:text-white" />
               ) : (
@@ -41,7 +42,7 @@ export default function Sidebar({ data }: Props) {
                 height: isOpen
                   ? `${
                       (ref.current?.scrollHeight &&
-                        ref.current?.scrollHeight * posts.length + 15) ||
+                        ref.current?.scrollHeight * posts.length) ||
                       0
                     }px`
                   : "0px",
@@ -49,16 +50,16 @@ export default function Sidebar({ data }: Props) {
             >
               <ul
                 ref={ref}
-                className="mt-1 space-y-1 pl-4 border-l border-border"
+                className="mt-1 ml-4 space-y-1 border-l border-border"
               >
                 {posts.map((post) => (
-                  <li key={post.slug}>
-                    <Link href={`/posts/${post.slug}`}>
-                      <span className="hover:underline text-gray cursor-pointer">
+                  <Link key={post.slug} href={`/posts/${post.slug}`}>
+                    <li className="w-full py-2 cursor-pointer group hover:bg-slate-100 dark:hover:bg-zinc-800">
+                      <span className="pl-4 group-hover:underline text-gray">
                         {post.title}
                       </span>
-                    </Link>
-                  </li>
+                    </li>
+                  </Link>
                 ))}
               </ul>
             </div>
