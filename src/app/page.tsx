b2ceb5250ab8324d030e3690a200c6aeec4d8694
choiseongho <chltjdgh3@naver.com>
2025-06-08 15:23:38 +0900

    fix: home 화면 server 컴포넌트로 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,36 @@
 import { getPostsByCategory } from "@/lib/getPostsByCategory";
-import ClientHome from "@/components/ClientHome";
+import { categoryMap } from "@/lib/categoryMap";
+import Link from "next/link";
 
 export default function Home() {
-  const postsByCategory = getPostsByCategory(); // 서버에서 동기적으로 호출
+  const postsByCategory = getPostsByCategory();
 
-  return <ClientHome data={postsByCategory} />;
+  return (
+    <main className="p-8">
+      <h1 className="text-2xl font-bold mb-6">글 목록</h1>
+      {Object.entries(postsByCategory).map(([category, posts]) => (
+        <div key={category} className="mb-4 border rounded">
+          <div className="w-full text-left px-4 py-2 bg-green-100 dark:bg-zinc-800 font-semibold rounded-t">
+            {categoryMap[category] ?? category}
+          </div>
+          {category && (
+            <ul className="pl-6 pr-4 py-2 space-y-2">
+              {posts.map((post) => (
+                <li key={post.slug}>
+                  <Link href={`/posts/${post.slug}`}>
+                    <div className="flex justify-between text-gray hover:underline cursor-pointer">
+                      {post.title}{" "}
+                      <span className="text-sm text-gray-500">
+                        ({post.date})
+                      </span>
+                    </div>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      ))}
+    </main>
+  );
 }
