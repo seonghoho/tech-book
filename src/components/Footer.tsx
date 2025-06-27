104830a89863c19e0e59717d36494f3350dce63f
seonghoho <seonghoho@codnut.com>
2025-06-27 12:54:41 +0900

    style: footer style 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,24 +1,26 @@
 const Footer = () => {
   return (
-    <footer className="w-full py-6 border-t border-border bg-white dark:bg-dark text-center text-sm text-gray-800 dark:text-gray-200">
-      <p>
-        © {new Date().getFullYear()} TechBook. All rights reserved.{" | "}
-        <a
-          href="https://github.com/seonghoho"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="underline hover:text-black dark:hover:text-white"
-        >
-          GitHub
-        </a>
-        {" | "}
-        <a
-          href="/about"
-          className="underline hover:text-black dark:hover:text-white"
-        >
-          Who Am I?
-        </a>
-      </p>
+    <footer className="w-full py-8 border-t border-border bg-white dark:bg-dark text-center text-sm text-gray-800 dark:text-gray-200">
+      <div className="flex flex-col gap-1">
+        <div>© {new Date().getFullYear()} TechBook. All rights reserved.</div>
+        <div>
+          <a
+            href="https://github.com/seonghoho"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline hover:text-black dark:hover:text-white"
+          >
+            GitHub
+          </a>
+          {" | "}
+          <a
+            href="/about"
+            className="underline hover:text-black dark:hover:text-white"
+          >
+            Who Am I?
+          </a>
+        </div>
+      </div>
     </footer>
   );
 };
