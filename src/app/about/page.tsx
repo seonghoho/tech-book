23c88f9c1b803654842205f7a51d4d25fd30a8f7
choiseongho <chltjdgh3@naver.com>
2025-06-29 23:22:15 +0900

    fix: 소개페이지 반응형 수정

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,21 +9,23 @@ const page = () => {
   ];
 
   return (
-    <div className="flex my-10 mx-auto justify-center ">
+    <div className="flex sm:flex-row flex-col my-10 mx-auto justify-center gap-4">
       <div className="flex flex-1 justify-center">
-        <Image
-          className="rounded-xl"
-          src="/images/Profile.JPG"
-          alt="프로필 사진"
-          width={300}
-          height={400}
-          priority
-        />
+        <div className="relative w-[250px] h-[300px] sm:w-[300px] sm:h-full">
+          <Image
+            className="rounded-xl object-cover"
+            src="/images/Profile.JPG"
+            alt="프로필 사진"
+            width={300}
+            height={400}
+            priority
+          />
+        </div>
       </div>
-      <div className="flex flex-col flex-1 justify-between py-14">
+      <div className="flex flex-col flex-1 mx-auto justify-center sm:gap-6 gap-2 ">
         <div>
-          <div className="text-[36px] font-semibold">최성호</div>
-          <div className="text-[20px]">FrontEnd Developer</div>
+          <div className="sm:text-[36px] text-[24px] font-semibold">최성호</div>
+          <div className="sm:text-[20px] text-[16px]">FrontEnd Developer</div>
         </div>
 
         <div>
@@ -39,9 +41,9 @@ const page = () => {
                 href={href}
                 target={data.type === "link" ? "_blank" : undefined}
                 rel={data.type === "link" ? "noopener noreferrer" : undefined}
-                className="flex items-center gap-2 mt-2 text-[16px] hover:underline"
+                className="flex items-center gap-2 mt-2 sm:text-[16px] text-[14px] hover:underline"
               >
-                <data.icon className="w-5 h-5" />
+                <data.icon className="sm:w-5 sm:h-5 w-4 h-4" />
                 <span>{data.content}</span>
               </a>
             );
