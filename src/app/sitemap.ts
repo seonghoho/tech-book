e794e72d8d149bdc573517360d9c81924ee9bcda
seonghoho <seonghoho@codnut.com>
2025-07-10 12:50:43 +0900

    fix: 가져올 수 없음으로 인한 배포 트리거 추가

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,6 +2,7 @@ import { type MetadataRoute } from "next";
 import posts from "../../public/posts.json";
 export const dynamic = "force-dynamic";
 export const revalidate = 3600; // 1시간(3600초)마다 자동 갱신
+// Google Search Console '가져올 수 없음' 오류 해결을 위한 재배포 트리거
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const routes = posts.map((post) => ({
