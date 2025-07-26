4fcb6f68a0b84707f9834009aa562dffbf5cd031
seonghoho <chltjdgh3@naver.com>
2025-07-27 01:06:57 +0900

    fix: pixel-runner 설명 추가

--- src/components/games/GameComponent.tsx
+++ src/components/games/GameComponent.tsx
@@ -25,7 +25,7 @@ const GameComponent = ({ gameName }: GameComponentProps) => {
   const handleStartGame = () => {
     setGameState("playing");
     if (gameInstanceRef.current) {
-      console.log(gameInstanceRef.current);
+      // console.log(gameInstanceRef.current);
       gameInstanceRef.current.start();
     }
   };
@@ -56,24 +56,59 @@ const GameComponent = ({ gameName }: GameComponentProps) => {
   };
 
   return (
-    <div
-      style={{
-        position: "relative",
-        width: "100%",
-        height: "100%",
-        border: "1px solid #ccc",
-      }}
-    >
-      {gameState === "ready" && <StartMenu onStart={handleStartGame} />}
-      {gameState === "gameOver" && (
-        <GameOverModal score={score} onRestart={handleRestartGame} />
-      )}
-      <GameClientWrapper
-        gameName={gameName}
-        onGameInstanceReady={onGameInstanceReady}
-        onGameOver={handleGameOver}
-        onScoreUpdate={handleScoreUpdate}
-      />
+    <div className="relative w-full h-full">
+      <div className="h-[400px]">
+        {gameState === "ready" && <StartMenu onStart={handleStartGame} />}
+        {gameState === "gameOver" && (
+          <GameOverModal score={score} onRestart={handleRestartGame} />
+        )}
+        <GameClientWrapper
+          gameName={gameName}
+          onGameInstanceReady={onGameInstanceReady}
+          onGameOver={handleGameOver}
+          onScoreUpdate={handleScoreUpdate}
+        />
+      </div>
+      <div className="flex flex-col gap-4">
+        <div className="font-press flex flex-col sm:flex-row items-center justify-center font-semibold text-lg py-2">
+          <span>캐릭터를 이동해 30초 동안 </span>
+          <span>장애물을 피해보세요!</span>
+        </div>
+        <div className="font-press flex flex-col lg:flex-row gap-4 justify-center items-center text-sm">
+          <div className="flex flex-col sm:flex-row gap-2 sm:gap-4 items-center justify-center sm:justify-between w-full sm:w-auto">
+            <div className="flex justify-between sm:gap-2 sm:w-auto w-48">
+              <span>이동</span>
+              <span>← →</span>
+            </div>
+            <div className="flex justify-between sm:gap-2 sm:w-auto w-48">
+              <span>점프</span>
+              <span>Space</span>
+            </div>
+            <div className="flex justify-between sm:gap-2 sm:w-auto w-48">
+              <span>공격</span>
+              <span>Z/X/C</span>
+            </div>
+          </div>
+          <div className="flex flex-col sm:flex-row gap-2 sm:gap-4 items-center justify-center sm:justify-between w-full sm:w-auto">
+            <div className="flex justify-between sm:gap-2 sm:w-auto w-48">
+              <span>쉴드</span>
+              <span>Q</span>
+            </div>
+            <div className="flex justify-between sm:gap-2 sm:w-auto w-48">
+              <span>캐릭터 선택</span>
+              <span>1/2/3</span>
+            </div>
+            <div className="flex justify-between sm:gap-2 sm:w-auto w-48">
+              <span>배경</span>
+              <span>Tab+1/Tab+2</span>
+            </div>
+            <div className="flex justify-between sm:gap-2 sm:w-auto w-48">
+              <span>재시작</span>
+              <span>R</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
